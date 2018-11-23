f43522e416fef0f0160428296969e9992a619c75
黄良运 <1136513099@qq.com>
2018-11-23 17:22:06 +0800

    spring-boot-vue

---
+++ spring-boot-vue/spring-boot-vue/src/main/java/com/hly/springbootvue/SpringBootVueApplication.java
@@ -0,0 +1,12 @@
+package com.hly.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVueApplication.class, args);
+	}
+}
