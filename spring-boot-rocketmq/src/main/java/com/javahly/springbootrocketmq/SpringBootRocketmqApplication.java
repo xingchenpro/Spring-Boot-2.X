07945c7d64abaf38dc0b1eb3172e49b15030b8c2
黄良运 <1136513099@qq.com>
2020-04-10 03:16:48 +0800

    rocketmq

---
+++ spring-boot-rocketmq/src/main/java/com/javahly/springbootrocketmq/SpringBootRocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.javahly.springbootrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRocketmqApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rocketmq/src/test/java/com/javahly/springbootrocketmq/SpringBootRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahly.springbootrocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRocketmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
