c29099964deb63f9e0d0771014e4f4b59c49b660
黄良运 <1136513099@qq.com>
2020-04-10 03:13:32 +0800

    kafka

---
+++ spring-boot-kafka/src/main/java/com/javahly/springbootkafka/SpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.javahly.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaApplication.class, args);
+	}
+
+}
